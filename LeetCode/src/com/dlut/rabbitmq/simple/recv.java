d3b774afe48c6070809f39bde61590ba5d5d15e9
fys <1113014250@qq.com>
2019-01-27 16:15:19 +0800

    2019-1-27 init of simpleQueue(new api)

--- LeetCode/src/com/dlut/rabbitmq/simple/recv.java
+++ LeetCode/src/com/dlut/rabbitmq/simple/recv.java
@@ -1,12 +1,11 @@
 package com.dlut.rabbitmq.simple;
 
 import com.dlut.rabbitmq.util.ConnectionUtil;
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.QueueingConsumer;
+import com.rabbitmq.client.*;
 import sun.nio.cs.ext.MacGreek;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 
 /**
  * 消费者消费消息
@@ -16,6 +15,36 @@ public class recv
     final static String queuename = "test_simple_queue";
 
     public static void main(String[] args) throws IOException, InterruptedException
+    {
+        newApi();
+        return;
+    }
+
+    private static void newApi() throws IOException
+    {
+        //获取链接
+        Connection connection = ConnectionUtil.getConnection();
+
+        //创建频道
+        Channel channel = connection.createChannel();
+
+        //队列声明
+        channel.queueDeclare(queuename,false,false,false,null);
+
+        DefaultConsumer defaultConsumer = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException
+            {
+                String strMsg = new String(body,"utf-8");
+                System.out.println("[recv] msg:" + strMsg);
+            }
+        };
+
+        //监听这个队列(这种写法类似于android的addlistener的方法)
+        channel.basicConsume(queuename,true,defaultConsumer);
+    }
+
+    private static void oldApi() throws IOException, InterruptedException
     {
         //获取链接
         Connection connection = ConnectionUtil.getConnection();
@@ -36,6 +65,5 @@ public class recv
             String strMsg = new String(body);
             System.out.println("[recv] msg:" + strMsg);
         }
-
     }
 }
